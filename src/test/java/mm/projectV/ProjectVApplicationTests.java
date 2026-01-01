d78221556bdd4bc1d9f784eeee4eecd4eaaec15a
Martynenko Maksym <super.super.maksim33@gmail.com>
2026-01-01 16:55:31 +0100

    first commit

---
+++ src/test/java/mm/projectV/ProjectVApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.projectV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
